230d61f2ca603e48d9a0817b7bf1fbb6889d913d
inventivetalent <mail@inventivetalent.org>
2018-07-17 20:45:06 +0200

    let's try a different token

--- .travis/push.sh
+++ .travis/push.sh
@@ -16,7 +16,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add origin https://InventiveBot:${GH_TOKEN}@github.com/InventivetalentDev/MineRender.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/InventivetalentDev/MineRender.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
 
