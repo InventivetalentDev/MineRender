4c778f2649fc37b101307ff85119c3a529cdef60
inventivetalent <mail@inventivetalent.org>
2018-07-17 20:19:22 +0200

    fix

--- .travis/push.sh
+++ .travis/push.sh
@@ -16,7 +16,7 @@ commit_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/InventivetalentDev/MineRender.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/InventivetalentDev/MineRender.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
 
