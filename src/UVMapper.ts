178a0cde926c62ec97b95008adf1279446955f05
inventivetalent <mail@inventivetalent.org>
2023-11-06 13:56:27 +0100

    don't add undefined assets

--- src/UVMapper.ts
+++ src/UVMapper.ts
@@ -259,10 +259,14 @@ export class UVMapper {
                     uniqueTextureNames.push(textureKey);
                     const assetKey = AssetKey.parse("textures", textureValue, model.key);
                     promises.push(ModelTextures.get(assetKey).then(asset => {
-                        textureMap[textureKey] = new WrappedImage(asset!);
+                        if (asset) {
+                            textureMap[textureKey] = new WrappedImage(asset);
+                        }
                     }));
                     promises.push(ModelTextures.getMeta(assetKey).then(meta => {
-                        metaMap[textureKey] = meta;
+                        if (meta) {
+                            metaMap[textureKey] = meta;
+                        }
                     }))
                 }
             }
